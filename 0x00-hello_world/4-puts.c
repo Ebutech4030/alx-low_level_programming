f15ae5fe116738be4e8b208e3fd31cc00786e6ec
ebutech4030 <ebubechiufomba@gmail.com>
2022-09-12 16:22:41 -0700

    putter

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,12 @@
 #!/bin/bash
-
-#include <stdio.h>
 /**
- * main - print the string in the put function
- *
- * Descriotion: using the main function
- * this program prints "programming is like building a multinational puzzle
- * Return :0
+ * main -entry point
+ * 
+ * Return; 0 after printing the function
  */
-int main(void)
+#include <stdio.h>
+int main (void)
 {
 	puts("\" programming is like building a multilingual puzzle");
 	return (0);
 }
-
